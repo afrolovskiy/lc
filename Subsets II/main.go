b3c65d17a33dda53346c67830ef512c8a6048684
afrolovskiy <frolovskiyav@gmail.com>
2019-12-24 10:52:58 +0300

    Speed up Subsets II

--- Subsets II/main.go
+++ Subsets II/main.go
@@ -8,38 +8,29 @@ import (
 )
 
 func main() {
-	// v := []int{1, 2, 2}
-	v := []int{1, 2, 2, 3}
+	v := []int{1, 2, 2}
+	// v := []int{1, 2, 2, 3}
 	sort.Ints(v)
 	fmt.Printf("%v\n", subsets(v))
 }
 
 func subsets(nums []int) [][]int {
-	if len(nums) == 0 {
-		return nil
-	}
-
 	var res [][]int
-	dups := make(map[string]bool)
+	dups := make(map[string]struct{})
+	for i := 0; i < 1<<uint(len(nums)); i++ {
+		ss := []int{}
+		for j := 0; j < len(nums); j++ {
+			if i&(1<<uint(j)) > 0 {
+				ss = append(ss, nums[j])
+			}
+		}
 
-	add := func(x []int) {
-		k := key(x)
-		if !dups[k] {
-			res = append(res, x)
-			dups[k] = true
+		k := key(ss)
+		if _, ok := dups[k]; !ok {
+			res = append(res, ss)
+			dups[k] = struct{}{}
 		}
 	}
-
-	for i := range nums {
-		add([]int{nums[i]})
-
-		ss := subsets(nums[i+1:])
-		for j := range ss {
-			add(ss[j])
-			add(append([]int{nums[i]}, ss[j]...))
-		}
-	}
-
 	return res
 }
 
